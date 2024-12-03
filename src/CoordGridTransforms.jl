f4f4922b78cc0a042b79c11cb14be2916dcb2f03
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2024-12-03 17:37:49 -0300

    Add DATADEPS_ALWAYS_ACCEPT

--- src/CoordGridTransforms.jl
+++ src/CoordGridTransforms.jl
@@ -15,6 +15,12 @@ using DataDeps: @datadep_str, register, DataDep
 
 import GeoTIFF
 
+function __init__()
+  # make sure datasets are always downloaded
+  # without user interaction from DataDeps.jl
+  ENV["DATADEPS_ALWAYS_ACCEPT"] = true
+end
+
 include("grids.jl")
 include("transforms.jl")
 
