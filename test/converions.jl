a27e7e2877ec08a3d849fa6d401d77448ff6ba66
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2024-12-12 17:18:10 -0300

    Handle coordinates that are outside the grid bounds (#15)
    * Handle coordinates that are outside the grid bounds
    
    * Finalize the implementation
    
    * Add tests
    
    * Add more tests

--- test/converions.jl
+++ test/converions.jl
@@ -263,7 +263,29 @@
   c3 = convert(LatLon{NTF}, c2)
   @test allapprox(c3, c1)
 
-  # error: coordinates outside of the transform domain
+  # coordinates outside the grid bounds
+  # hgridshift
+  c1 = LatLon{SAD96}(T(10), T(-10))
+  c2 = convert(LatLon{SIRGAS2000}, c1)
+  @test c2.lat ≈ c1.lat
+  @test c2.lon ≈ c1.lon
+
   c1 = LatLon{RD83}(T(65), T(25))
-  @test_throws ArgumentError convert(LatLon{ETRFLatest}, c1)
+  c2 = convert(LatLon{ETRFLatest}, c1)
+  @test c2.lat ≈ c1.lat
+  @test c2.lon ≈ c1.lon
+
+  # pointmotion
+  c1 = LatLonAlt{NAD83CSRS{3}}(T(90), T(10), T(1))
+  c2 = convert(LatLonAlt{NAD83CSRS{4}}, c1)
+  @test c2.lat ≈ c1.lat
+  @test c2.lon ≈ c1.lon
+  @test c2.alt ≈ c1.alt
+
+  # geocgridtranslation
+  c1 = convert(Cartesian, LatLon{NTF}(T(65), T(25)))
+  c2 = convert(Cartesian{RGF93v1}, c1)
+  @test c2.x ≈ c1.x
+  @test c2.y ≈ c1.y
+  @test c2.z ≈ c1.z
 end
