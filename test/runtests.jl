932e0cfac0cbee58d749a38f63056ceaf8fc757f
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-06-12 08:38:08 -0300

    Set float type in GitHub action

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,26 +4,26 @@ using Test
 
 using Unitful: °
 
-testfiles = ["conversions.jl"]
+# environment settings
+isCI = "CI" ∈ keys(ENV)
+islinux = Sys.islinux()
+visualtests = !isCI || (isCI && islinux)
+datadir = joinpath(@__DIR__, "data")
 
-# --------------------------------
-# RUN TESTS WITH SINGLE PRECISION
-# --------------------------------
-
-T = Float32
-@testset "CoordGridTransforms.jl ($T)" begin
-  for testfile in testfiles
-    println("Testing $testfile...")
-    include(testfile)
+# float settings
+T = if isCI
+  if ENV["FLOAT_TYPE"] == "Float32"
+    Float32
+  elseif ENV["FLOAT_TYPE"] == "Float64"
+    Float64
   end
+else
+  Float64
 end
 
-# --------------------------------
-# RUN TESTS WITH DOUBLE PRECISION
-# --------------------------------
+testfiles = ["conversions.jl"]
 
-T = Float64
-@testset "CoordGridTransforms.jl ($T)" begin
+@testset "CoordGridTransforms.jl" begin
   for testfile in testfiles
     println("Testing $testfile...")
     include(testfile)
